6c99201868b4ccaa98920c9050e3232b3419f388
sibisaravanan1 <sibisaravanan@presidio.com>
2022-04-22 12:25:16 +0530

    add variable description

--- variables.tf
+++ variables.tf
@@ -1,16 +1,19 @@
 variable "aws_region" {
   type    = string
   default = "us-east-1"
+  description = "AWS Region"
 }
 
 variable "vpc_name" {
   type    = string
-  default = "demo_vpc"
+  default = "my_vpc"
+  description = "Desired name for the VPC"
 }
 
 variable "cidr_block" {
   type    = string
   default = "10.0.0.0/16"
+  description = "CIDR Range"
 }
 
 variable "private_subnets" {
@@ -20,6 +23,7 @@ variable "private_subnets" {
     "private_subnet_2" = 2
     "private_subnet_3" = 3
   }
+  description = "Map of private subnets"
 }
 variable "public_subnets" {
   type = map(string)
@@ -28,20 +32,24 @@ variable "public_subnets" {
     "public_subnet_2" = 2
     "public_subnet_3" = 3
   }
+  description = "Map of private subnets"
 }
 
 variable "instance_tenancy" {
   type = string
   default = "default"
+  description = "Instance Tenancy"
 }
 
 variable "enable_dns_hostnames" {
   type = bool
   default = true
+  description = "Enable DNS Hostnames"
 }
 
 variable "enable_dns_support" {
   type = bool
   default = true
+  description = "Enable DNS Support"
 }
 
