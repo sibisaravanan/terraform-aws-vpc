1629a45635bca9ceeb82f400df64389b3cfa617b
sibisaravanan1 <sibisaravanan@presidio.com>
2022-04-22 12:04:11 +0530

    add module

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # terraform-aws-vpc
+
+## Usage
+    module "vpc" {
+       source = "/Users/sibi/Devops/repositories/  terraform-aws-vpc"
+     
+       vpc_name              = "my_vpc"  
+       cidr_block           = "10.0.0.0/16"  
+       instance_tenancy     = "default"  
+       enable_dns_hostnames = true  
+       enable_dns_support   = true  
+    }  
---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "demo_vpc"
+}
+
+variable "cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  type = map(string)
+  default = {
+    "private_subnet_1" = 1
+    "private_subnet_2" = 2
+    "private_subnet_3" = 3
+  }
+}
+variable "public_subnets" {
+  type = map(string)
+  default = {
+    "public_subnet_1" = 1
+    "public_subnet_2" = 2
+    "public_subnet_3" = 3
+  }
+}
+
+variable "instance_tenancy" {
+  type = string
+  default = "default"
+}
+
+variable "enable_dns_hostnames" {
+  type = bool
+  default = true
+}
+
+variable "enable_dns_support" {
+  type = bool
+  default = true
+}
+
